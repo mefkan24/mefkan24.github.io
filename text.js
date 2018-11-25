d9f2f18dcbcf365244be59d3bc26964378d091b7
mefkan24 <43763363+mefkan24@users.noreply.github.com>
2018-11-26 02:20:41 +0300

    test
    testt

---
+++ text.js
@@ -0,0 +1,4 @@
+alert(document.getElementById("users_credentials_password_field_group_password_").value)
+alert('I know,I just got your password in MD5 hash')
+confirm('But wait...')
+alert('I am a good guy,no worries,just changed your password to hijackedaccount')
